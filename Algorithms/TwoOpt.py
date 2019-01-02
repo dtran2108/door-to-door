f91b11d783488b04a76f79d8a782b469790c877a
Trân Dương <dtran2108@gmail.com>
2019-01-03 02:01:51 +0700

    move Parsers/swap to twoopt.py

--- Algorithms/TwoOpt.py
+++ Algorithms/TwoOpt.py
@@ -1,4 +1,4 @@
-from Parsers import Swap
+from Parsers import total_length
 from time import time
 from graph import Graph
 
@@ -14,3 +14,22 @@ class Two_opt(Graph):
         best, road_length = Swap(route, best)
         end_time = time()
         return [city for city in best], len(best), road_length, end_time-start_time
+
+    def Swap(route, best, improved=True):
+    # loop until no better route is found
+    while improved:
+        improved = False
+        for i in range(1, len(route)):
+            for j in range(i+1, len(route)):
+                # changes nothing, skip then
+                if j-i == 1:
+                    continue
+                new_route = route[:]
+                # this is the 2woptSwap
+                new_route[i:j] = route[j-1:i-1:-1]
+                if total_length(new_route) < total_length(best):
+                    best = new_route
+                    # better route is found
+                    improved = True
+        route = best
+    return best, total_length(best)
--- Parsers/methods.py
+++ Parsers/methods.py
@@ -1,26 +1,6 @@
 from node import Node
 
 
-def Swap(route, best, improved=True):
-    # loop until no better route is found
-    while improved:
-        improved = False
-        for i in range(1, len(route)):
-            for j in range(i+1, len(route)):
-                # changes nothing, skip then
-                if j-i == 1:
-                    continue
-                new_route = route[:]
-                # this is the 2woptSwap
-                new_route[i:j] = route[j-1:i-1:-1]
-                if total_length(new_route) < total_length(best):
-                    best = new_route
-                    # better route is found
-                    improved = True
-        route = best
-    return best, total_length(best)
-
-
 def total_length(route):
     """ calculate the total length of the route """
     road_length = 0
